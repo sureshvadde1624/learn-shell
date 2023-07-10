66a6ab632cda5193d069e3a9cdae056906d3f490
suresh <thesureshvadde@gmail.com>
2023-07-10 20:42:37 +0530

    var in function

--- 05-functions.sh
+++ 05-functions.sh
@@ -6,3 +6,34 @@ xyz() {
 # call a function
 xyz
 
+# you can also send inputs to the functions
+ex() {
+  echo Hello From Function
+  echo First Argument - $1
+  echo Second Argument - $2
+  echo All Arguments - $*
+  echo  Number of Argument - $#
+}
+
+ex first second #---these inputs are applicable only for function
+
+## Variable accessing inside and outside of the function
+ex2() {
+  echo value of a = ${a}
+  b=20
+}
+a=10
+ex2
+echo value of b = ${b}
+
+## updating value of var inside the function
+ex3(){
+  a=20
+  echo value of a = ${a}
+  b=30
+}                                   ## variables can be override whether it is inside or outside of the function
+a=10
+ex3
+b=40
+echo value of b = ${b}
+
