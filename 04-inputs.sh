112be39744a71e5b0faadb55bce6db7e6bd4210e
suresh <thesureshvadde@gmail.com>
2023-07-10 19:53:49 +0530

    special variables

---
+++ 04-inputs.sh
@@ -0,0 +1,10 @@
+# $0-$n  ; $*/$@ ; $# --> these are special variables.u cant declare them as vars.
+echo $1       # First Argument
+echo $2       #Second Argument
+
+echo $0       #Script Name
+
+echo $*       #All the Arguments
+echo $@       #All the Arguments
+
+echo $#       #Number of Arguments
