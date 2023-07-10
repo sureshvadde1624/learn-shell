f11c4cbdd49af10a1ff6c84d8b772f0348e1be91
suresh <thesureshvadde@gmail.com>
2023-07-10 19:44:05 +0530

    vars declaration

--- 03-vars.sh
+++ 03-vars.sh
@@ -1,2 +1,12 @@
+#VAR=DATA---->$VAR or ${VAR}
 DATE=2023-07-10
 echo "Hello, Good Evening, Today Date is ${DATE}"
+
+#COMMAND SUBSTITUTION      VAR=$(COMMAND)
+DATE=$(date)
+echo "Hello, Good Evening, Today Date is ${DATE}"
+
+#Arthimatic substition      VAR=$((EXPRESSION))
+ADD=$((2+4))
+echo "addition of 2+4 = ${ADD}"
+
